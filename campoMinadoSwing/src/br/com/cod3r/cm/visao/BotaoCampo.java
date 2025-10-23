cdcb01e14ab51984b2b06dba7cd85c6fbd9d14a4
Emily <emilycxt2015@gmail.com>
2025-10-23 19:04:50 -0300

    trabalhando na classe BotaoCampo, definindo cores etc

--- campoMinadoSwing/src/br/com/cod3r/cm/visao/BotaoCampo.java
+++ campoMinadoSwing/src/br/com/cod3r/cm/visao/BotaoCampo.java
@@ -1,5 +1,8 @@
 package br.com.cod3r.cm.visao;
 
+import java.awt.Color;
+
+import javax.swing.BorderFactory;
 import javax.swing.JButton;
 
 import br.com.cod3r.cm.modelo.Campo;
@@ -9,8 +12,19 @@ import br.com.cod3r.cm.modelo.CampoObservador;
 @SuppressWarnings("serial")
 public class BotaoCampo extends JButton implements CampoObservador{
 	
+	private final Color BG_PADRAO = new Color (184, 184, 184);
+	private final Color BG_MARCADO = new Color (8, 1, 247);
+	private final Color BG_EXPLODIR = new Color (189, 66, 68);
+	private final Color TEXTO_VERDE = new Color (0, 100, 0);
+	
+	private Campo campo;
+	
 	public BotaoCampo (Campo campo) {
+		this.campo = campo;
+		setBackground (BG_PADRAO);
+		setBorder(BorderFactory.createBevelBorder(0));
 		
+		campo.registrarObservador(this);
 	}
 
 	@Override
@@ -25,22 +39,28 @@ public class BotaoCampo extends JButton implements CampoObservador{
 		case EXPLODIR:
 			aplicarEstiloExplodir();
 			break;
+		default: aplicarEstiloPadrao();	
 		}
 		
 	}
 
+	private void aplicarEstiloPadrao() {
+		
+		
+	}
+
 	private void aplicarEstiloExplodir() {
-		// TODO Auto-generated method stub
+		
 		
 	}
 
 	private void aplicarEstiloMarcar() {
-		// TODO Auto-generated method stub
+		
 		
 	}
 
 	private void aplicarEstiloAbrir() {
-		// TODO Auto-generated method stub
+		
 		
 	}
 	
