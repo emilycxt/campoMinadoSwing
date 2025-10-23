8cf8cc7aaccd4720f5a9f7adf85dea738fa475ea
Emily <emilycxt2015@gmail.com>
2025-10-23 16:22:25 -0300

    criação de Enum e nova interface padrão observador

--- campoMinadoSwing/src/br/com/cod3r/cm/modelo/Campo.java
+++ campoMinadoSwing/src/br/com/cod3r/cm/modelo/Campo.java
@@ -14,10 +14,20 @@ public class Campo {
 	private boolean marcado = false;
 
 	private List<Campo> vizinhos = new ArrayList<>();
+	private List<CampoObservador> observadores = new ArrayList<>();
 
 	public Campo(int linha, int coluna) {
 		this.linha = linha;
 		this.coluna = coluna;
+	}
+	
+	public void registrarObservador(CampoObservador observador) {
+		observadores.add(observador);
+	}
+	
+	private void notificarObservadores(CampoEvento evento) {
+		observadores.stream()
+		.forEach(o -> o.eventoOcorreu(this, evento));
 	}
 
 	public boolean adicionarVizinho(Campo vizinho) {
@@ -43,16 +53,24 @@ public class Campo {
 	void alternarMarcacao() {
 		if (!aberto) {
 			marcado = !marcado;
+			
+			if (marcado) {
+				notificarObservadores(CampoEvento.MARCAR);
+			}else {
+				notificarObservadores(CampoEvento.DESMARCAR);
+			}
 		}
 	}
 
 	boolean abrir() {
 		if (!aberto && !marcado) {
-			aberto = true;
 
 			if (minado) {
-				//TODO Implementar nova versão
+				notificarObservadores(CampoEvento.EXPLODIR);
+				return true;
 			}
+			setAberto(true);
+						
 			if (vizinhancaSegura()) {
 				vizinhos.forEach(v -> v.abrir());
 			}
@@ -76,6 +94,10 @@ public class Campo {
 
 	void setAberto (boolean aberto) {
 		this.aberto = aberto;
+		
+		if (aberto) {
+		notificarObservadores(CampoEvento.ABRIR);
+		}
 	}
 	
 	public boolean isAberto() {
