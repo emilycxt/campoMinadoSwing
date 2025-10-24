f4d60435c9ea2f3de5be4e43e1fae42e2b85431d
Emily <emilycxt2015@gmail.com>
2025-10-24 09:40:09 -0300

    colocando menuzinho de escolha para dificuldade

--- campoMinadoSwing/src/br/com/cod3r/cm/visao/TelaPrincipal.java
+++ campoMinadoSwing/src/br/com/cod3r/cm/visao/TelaPrincipal.java
@@ -6,23 +6,65 @@ import br.com.cod3r.cm.modelo.Tabuleiro;
 
 @SuppressWarnings("serial")
 public class TelaPrincipal extends JFrame {
-	
+
 	public TelaPrincipal() {
-		Tabuleiro tabuleiro = new Tabuleiro (16, 30, 60);
+		String[] opcoes = { "Fácil", "Médio", "Difícil" };
+
+		int escolha = javax.swing.JOptionPane.showOptionDialog(null, "Escolha a dificuldade", "Campo Minado",
+				javax.swing.JOptionPane.DEFAULT_OPTION, javax.swing.JOptionPane.INFORMATION_MESSAGE, null, opcoes,
+				opcoes[0]);
+		int linhas = 0;
+		int colunas = 0;
+		int minas = 0;
+
+		switch (escolha) {
+		case 0: {
+			linhas = 9;
+			colunas = 9;
+			minas = 10;
+			break;
+		}
+		case 1: {
+			linhas = 16;
+			colunas = 16;
+			minas = 40;
+			break;
+		}
+		case 2: {
+			linhas = 16;
+			colunas = 30;
+			minas = 80;
+			break;
+		}
+		default: {
+			System.exit(0);
+		}
+		}
+		
+		Tabuleiro tabuleiro = new Tabuleiro (linhas, colunas, minas);
 		add (new PainelTabuleiro(tabuleiro));
 		
-		
 		setTitle("Campo Minado");
-		setSize(690, 438);
+		setSize (690, 438);
 		setLocationRelativeTo(null);
 		setDefaultCloseOperation(DISPOSE_ON_CLOSE);
-		setVisible(true);
+		setVisible (true);
+
 	}
-	
+
+	/*
+	 * Construtor 1.0 public TelaPrincipal() { Tabuleiro tabuleiro = new Tabuleiro
+	 * (16, 30, 60); add (new PainelTabuleiro(tabuleiro));
+	 * 
+	 * 
+	 * setTitle("Campo Minado"); setSize(690, 438); setLocationRelativeTo(null);
+	 * setDefaultCloseOperation(DISPOSE_ON_CLOSE); setVisible(true); }
+	 */
+
 	public static void main(String[] args) {
-		
+
 		new TelaPrincipal();
-		
+
 	}
 
 }
